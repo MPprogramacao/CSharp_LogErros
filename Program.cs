677e661b4f3d3f3441ef9f3340678e80f2354a46
Matheus Pimentel <42476165+MPprogramacao@users.noreply.github.com>
2021-07-04 15:08:24 -0300

    Importando arquivos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace CSharp_LogErros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistraLog.Log(String.Format($"{"Log criado em "} : {DateTime.Now}"), "ArquivoLog");
+            RegistraLog.Log("Registro de log de auditoria");
+
+           Console.WriteLine("Log criado e registrado com sucesso");
+           Console.ReadLine();
+        }
+    }
+}
